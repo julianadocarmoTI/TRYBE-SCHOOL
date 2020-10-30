33e6a3fd75db49ed017aee84bb7af7955640079b
Juliana do Carmo <julianadocarmoti@gmail.com>
2020-10-30 10:21:18 -0300

    project to do list

--- Projects/5 - To-do List/exercise_1.js
+++
@@ -1,8 +0,0 @@
-//BLOCK 4.2 - JavaScript ARRAY e Loop FOR 
-/* 1) Percorra o array imprimindo todos os valores nele contidos com a função console.log();
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-*/
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-for (index = 0; index < numbers.length; index++){
-    console.log(numbers[index]); 
-}
