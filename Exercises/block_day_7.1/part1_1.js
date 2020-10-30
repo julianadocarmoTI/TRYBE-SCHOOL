ef2a01d7e3ad689ec3abdaa3b7b32a974d13e4ac
Juliana do Carmo <julianadocarmoti@gmail.com>
2020-10-30 17:13:35 -0300

    exercises part1 - block 7.1

---
+++ Exercises/block_day_7.1/part1_1.js
@@ -0,0 +1,29 @@
+/* Corrija o código para que ideclarado na instrução if seja uma variável separada daquela declarada na iprimeira linha da função. Certifique-se de não usar a varpalavra - chave em nenhum lugar do código.
+
+Este exercício foi elaborado para ilustrar a diferença entre como vare as letpalavras - chave atribuem escopo à variável declarada. Ao programar uma função semelhante à usada neste exercício, geralmente é melhor usar nomes de variáveis ​​diferentes para evitar confusão.
+
+function checkScope() {
+    'use strict';
+    let i = 'function scope';
+    if (true) {
+      let i = 'block scope';
+      console.log('Block scope i is: ', i);
+    }
+         i = 'function scope'
+    console.log('Function scope i is: ', i);
+    return i;
+  }
+  */
+
+  function checkScope() {
+  'use strict'
+  let i = 'function scope'
+
+  if (true) {
+    let i = 'block scope'
+    console.log('Block scope i is: ', i)
+  }
+  i = 'function scope'
+  console.log('Function scope i is: ', i)
+  return i
+}
---
+++ Exercises/block_day_7.1/part1_2.js
@@ -0,0 +1,20 @@
+// Uma matriz é declarada como const s = [5, 7, 2]. Altere a matriz para [2, 5, 7]usar várias atribuições de elemento, e imprima na tela.
+
+// const s = [5, 7, 2];
+// function editInPlace() {
+//  'use strict';
+     // Only change code below this line
+     // Using s = [2, 5, 7] would be invalid
+     // Only change code above this line
+// }
+// editInPlace();
+
+const s = [5, 7, 2];
+function editInPlace() {
+  'use strict';
+s[0] = 2;
+s[1] = 5;
+s[2] = 7;
+}
+editInPlace();
+console.log(s);
